adf43c93dbf7ab77bcdc2df90dba9c02b378f190
MaryKateMagyar <marykatemagyar@gmail.com>
2025-04-25 16:11:12 -0700

    tests

--- tests.py
+++ tests.py
@@ -15,5 +15,51 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_create_cells2(self):
+        num_cols = 1
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_3(self):
+        num_cols = 20
+        num_rows = 25
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_cell_walls_initialization(self):
+        num_cols = 5
+        num_rows = 5
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for i in range(num_cols):
+            for j in range(num_rows):
+                cell = m1._cells[i][j]
+                self.assertTrue(cell.has_left_wall)
+                self.assertTrue(cell.has_right_wall)
+                self.assertTrue(cell.has_top_wall)
+                self.assertTrue(cell.has_bottom_wall)
+
+    def test_cell_dimensions(self):
+        cell_size_x = 15
+        cell_size_y = 20
+        m1 = Maze(0, 0, 5, 5, cell_size_x, cell_size_y)
+        cell = m1._cells[2][3]
+        self.assertEqual((cell._x2 - cell._x1), cell_size_x)
+        self.assertEqual((cell._y2 - cell._y1), cell_size_y)
+
 if __name__ == "__main__":
     unittest.main()
